8a2535dde911bac842c1efd102e6b473795efe7e
Qatrunnnn <qatrunraihan1@gmail.com>
2022-12-20 10:50:08 +0700

    initiate comit project

---
+++ AppsBangunDatar/src/main/java/com/mycompany/appsbangundatar/BangunDatar.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.appsbangundatar;
+
+/**
+ *
+ * @author user
+ */
+public class BangunDatar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
